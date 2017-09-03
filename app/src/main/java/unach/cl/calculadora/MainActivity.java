ceea48bd07b8de0a2545226e2ecc9bd2ff481217
omarinccjcc <omarinccjcc@hotmail.com>
2017-09-03 16:46:52 -0500

    add calculator

--- app/src/main/java/unach/cl/calculadora/MainActivity.java
+++ app/src/main/java/unach/cl/calculadora/MainActivity.java
@@ -5,9 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView txtPantalla;
+    double primeraVariable;
+    double segundaVariable;
+    String operacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        txtPantalla = (TextView)findViewById(R.id.txtPantalla);
+        operacion = "";
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +35,78 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void borrar(View v){
+        operacion = "";
+        primeraVariable = 0;
+        segundaVariable = 0;
+        txtPantalla.setText( "");
+    }
+    public void suma(View v){
+        primeraVariable = Double.parseDouble(txtPantalla.getText().toString());
+        txtPantalla.setText("");
+        operacion = "+";
+    }
+    public void resta(View v){
+        primeraVariable = Double.parseDouble(txtPantalla.getText().toString());
+        txtPantalla.setText("");
+        operacion = "-";
+    }
+    public void division(View v){
+        primeraVariable = Double.parseDouble(txtPantalla.getText().toString());
+        txtPantalla.setText("");
+        operacion = "/";
+    }
+    public void multiplicacion(View v){
+        primeraVariable = Double.parseDouble(txtPantalla.getText().toString());
+        txtPantalla.setText("");
+        operacion = "*";
+    }
+    public void igual(View v){
+        double resultado = 0;
+        segundaVariable = Double.parseDouble(txtPantalla.getText().toString());
+        if (operacion.equals("+")){
+            resultado = primeraVariable + segundaVariable;
+        }else if (operacion.equals("-")){
+            resultado = primeraVariable - segundaVariable;
+        }else if (operacion.equals("/")){
+            resultado = primeraVariable / segundaVariable;
+        }else if (operacion.equals("*")){
+            resultado = primeraVariable * segundaVariable;
+        }
+        txtPantalla.setText(resultado+"");
+    }
+
+    public void numero9(View v){
+        txtPantalla.setText( txtPantalla.getText().toString()+"9" );
+    }
+    public void numero8(View v){
+        txtPantalla.setText( txtPantalla.getText().toString()+"8" );
+    }
+    public void numero7(View v){
+        txtPantalla.setText( txtPantalla.getText().toString()+"7" );
+    }
+    public void numero6(View v){
+        txtPantalla.setText( txtPantalla.getText().toString()+"6" );
+    }
+    public void numero5(View v){
+        txtPantalla.setText( txtPantalla.getText().toString()+"5" );
+    }
+    public void numero4(View v){
+        txtPantalla.setText( txtPantalla.getText().toString()+"4" );
+    }
+    public void numero3(View v){
+        txtPantalla.setText( txtPantalla.getText().toString()+"3" );
+    }
+    public void numero2(View v){
+        txtPantalla.setText( txtPantalla.getText().toString()+"2" );
+    }
+    public void numero1(View v){
+        txtPantalla.setText( txtPantalla.getText().toString()+"1" );
+    }
+    public void numero0(View v){
+        txtPantalla.setText( txtPantalla.getText().toString()+"0" );
     }
 
 }
